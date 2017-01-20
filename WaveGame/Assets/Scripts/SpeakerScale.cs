557a44627ad2985bbd8ce387c767eeb15bff927d
Risto Haila <grest.hyber@gmail.com>
2017-01-21 01:45:46 +0200

    Added pointer movement, some graphics and the speaker

---
+++ WaveGame/Assets/Scripts/SpeakerScale.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeakerScale : MonoBehaviour
+{
+    float scale;
+
+    void Update()
+    {
+        scale = 0.5f + Mathf.PingPong(1 * Time.time, .5f/*speed*/) * 0.25f/*scale pingpong*/;
+        transform.localScale = new Vector3(scale, scale, 1);
+        
+    }
+}
