f5fe3c0d095b36f6b943ff37f7e45038686e0e7a
Risto Haila <grest.hyber@gmail.com>
2017-01-21 14:58:50 +0200

    pointer Smoother

--- WaveGame/Assets/Scripts/Pointer.cs
+++ WaveGame/Assets/Scripts/Pointer.cs
@@ -9,37 +9,34 @@ public class Pointer : MonoBehaviour
     public float MinPos = -1.89f;
     public float MaxPos = 2.5f;
     public float CurPos;
+    public float TargetPos;
     public float Sensitivity = 0.1f;
 
 
     void Start()
     {
-        CurPos = MinPos;
+        TargetPos = MinPos;
         Camera.main.aspect = 16 / 10;
         Debug.Log(Camera.main.pixelRect);
     }
 
     void Update()
     {
-        if (CurPos > MaxPos)
-        {
-            CurPos = MaxPos;
-        }
-        if (CurPos < MinPos)
-        {
-            CurPos = MinPos;
-        }
-
-        transform.position = new Vector3(transform.position.x, CurPos, 1);
-
         if (Input.GetKey(KeyCode.Space))
         {
-            CurPos += Sensitivity;
+            TargetPos += Sensitivity;
         }
         else
         {
-            CurPos -= Sensitivity;
+            TargetPos -= Sensitivity;
         }
+        TargetPos = Mathf.Clamp(TargetPos, MinPos, MaxPos);
+    }
+
+    void FixedUpdate()
+    {
+        CurPos = CurPos + (TargetPos - CurPos) * 0.1f;
+        transform.position = new Vector3(transform.position.x, CurPos, 1);
     }
 
 }
