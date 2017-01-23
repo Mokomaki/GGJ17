bbead2a01146d39986defac605b80ab753c94d81
Risto Haila <grest.hyber@gmail.com>
2017-01-24 00:27:30 +0200

    Added highscore system modfied the menu added the wave forms that were previusly unused added gameover screen when you die

---
+++ WaveGame/Assets/Scripts/GameOver.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOver : MonoBehaviour
+{
+    public static GameOver GoInctance;
+
+    void Awake()
+    {
+        if (GoInctance != this)
+            GoInctance = this;
+
+        Time.timeScale = 0;
+
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(0);
+            Time.timeScale = 1;
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            SceneManager.LoadScene(1);
+            Time.timeScale = 1;
+        }
+    }
+
+    void Start()
+    {
+        if (GoInctance != this)
+            GoInctance = this;
+    }
+}
--- WaveGame/Assets/Scripts/Heart.cs
+++ WaveGame/Assets/Scripts/Heart.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 
 public class Heart : MonoBehaviour
 {
+    public GameObject DisableME;
+    public GameObject GameOver;
+
     public static Heart Instance;
 
     public HeartSpriteHandlerOne Heart_1;
@@ -45,7 +48,18 @@ public class Heart : MonoBehaviour
         }
         else
         {
-            SceneManager.LoadScene(0);
+            gameObject.transform.position = new Vector3(10000,1000,1000);
+            DisableME.gameObject.transform.position = new Vector3(10000, 1000, 1000);
+            GameOver.SetActive(isActiveAndEnabled);
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                SceneManager.LoadScene(0);
+            }
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+                SceneManager.LoadScene(1);
+            }
         }
     }
 }
